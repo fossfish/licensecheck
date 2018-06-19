1baf036f37e7a8f8aa29e8bc24f5e47e570ac6d4
Andy Williams <andy@andywilliams.me>
2018-06-19 15:00:46 +0100

    Basic site template for Go and AppEngine

---
+++ README.md
@@ -0,0 +1,5 @@
+# License Check website for exploring open source license compliance
+
+This was generated from a template for Go, Bootstrap and AppEngine.
+
+More to come soon...
---
+++ welcome.go
@@ -0,0 +1,19 @@
+package licensecheck
+
+import (
+	"net/http"
+)
+
+// A Welcome message with title, demonstrates passing data to a template
+type Welcome struct {
+	Title   string
+	Message string
+}
+
+// A template taking a struct pointer (&message) containing data to render
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	message := Welcome{Title: "licensecheck.io", Message: "Welcome to your template site"}
+
+	// outerTheme refernces the template defined within theme.html
+	templates["welcome.html"].ExecuteTemplate(w, "outerTheme", &message)
+}
